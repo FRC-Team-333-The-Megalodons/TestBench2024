30b62c19fdefa7b6291ef62b3a971c7efe65692f
amina7mona <114757533+amina7mona@users.noreply.github.com>
2024-01-25 22:59:51 -0500

    encoder added

--- src/main/java/frc/robot/subsystems/FlexMotor.java
+++ src/main/java/frc/robot/subsystems/FlexMotor.java
@@ -8,7 +8,9 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,7 +18,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
     public class FlexMotor extends SubsystemBase{
     private CANSparkFlex flexMotor1;
     private CANSparkFlex flexMotor2;
-    //private CANSparkFlex flexMotor3;
+    private CANSparkFlex flexMotor3;
+    DutyCycleEncoder encoder;
+    PIDController pidThing = new PIDController(1.1, 0, 0);
+
     private DigitalInput pe;
 
     public FlexMotor() {
@@ -26,10 +31,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
         flexMotor2 = new CANSparkFlex(8, MotorType.kBrushless);
         flexMotor2.setIdleMode(IdleMode.kCoast);
 
-        //flexMotor3 = new CANSparkFlex(7, MotorType.kBrushless);
-        //flexMotor3.setIdleMode(IdleMode.kBrake);
+        flexMotor3 = new CANSparkFlex(7, MotorType.kBrushless);
+        flexMotor3.setIdleMode(IdleMode.kCoast);
+        encoder = new DutyCycleEncoder(0);
+        encoder.setConnectedFrequencyThreshold(900);
+        encoder.reset();
 
         pe = new DigitalInput(3);
+        pidThing.enableContinuousInput(0, 1);
     }
     
     public void oneForward(){flexMotor1.set(0.3);}
@@ -43,11 +52,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
     public void motorStop(){flexMotor1.set(0); flexMotor2.set(0);}
 
+    public void toSetPoint(){
+        flexMotor3.set(pidThing.calculate(encoder.getAbsolutePosition(), 0.5)); 
+    }
+
+    public void setToZero(){
+        flexMotor3.set(pidThing.calculate(encoder.getAbsolutePosition(), 0));
+    }
+
 
     public boolean detect() {
         if (pe.get()) {
             return true;
         } else { return false;  }
+    }
+
+    public boolean atSetPoint() {
+        if (encoder.getAbsolutePosition() == 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /*public void intakeIn() {
@@ -58,7 +83,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
     public void periodic(){
         SmartDashboard.putBoolean("NODE?", detect());
         //intakeIn();
-    
-    }
+        SmartDashboard.putNumber("encoder", encoder.get());
+        SmartDashboard.getBoolean("Encoder at positoin?", atSetPoint());
 }
- 
+    }
